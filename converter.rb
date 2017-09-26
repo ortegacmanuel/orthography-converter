43a1314cd4ce93697acf539b88080b87f3ab9cb8
Manuel Ortega <manuel@lasindias.coop>
2017-09-26 20:51:15 +0200

    using empty? instead of .count > 0

--- converter.rb
+++ converter.rb
@@ -1,22 +1,19 @@
-#!/usr/bin/env ruby
-require "Hunspell" # inject Hunspell class to Ruby namespace
+require 'Hunspell' # inject Hunspell class to Ruby namespace
 require 'csv'
 
-sp = Hunspell.new("Papiamento.aff", "Papiamento.dic")
-src_dir = "source.csv"
-dst_dir = "out.csv"
+sp = Hunspell.new('Papiamento.aff', 'Papiamento.dic')
+src_dir = 'source.csv'
+dst_dir = 'out.csv'
 puts " Reading data from  : #{src_dir}"
 puts " Writing data to    : #{dst_dir}"
-#create a new file
+# create a new file
 csv_out = File.open(dst_dir, 'wb')
-#read from existing file
-CSV.foreach(src_dir , :headers => true) do |row|
+# read from existing file
+CSV.foreach(src_dir, headers: true) do |row|
   if sp.spellcheck(row['pap_cw'])
     csv_out << row
-  elsif sp.suggest(row['pap_cw']).count > 0
+  elsif !sp.suggest(row['pap_cw']).empty?
     row[0] = sp.suggest(row['pap_cw']).first
-    # This items coming form automated suggestions needs to be checked
-    row[1] =  'check this'
     csv_out << row
   else
     row[0] = 'x'
